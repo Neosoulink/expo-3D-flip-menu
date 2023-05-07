87c4d0fd6bb4fdd15669c4086cf5e703391e58c6
Neosoulink <neodarksoulink@gmail.com>
2023-05-07 21:41:46 +0300

    chore: implement `three` support

--- babel.config.js
+++ babel.config.js
@@ -16,6 +16,7 @@ module.exports = function (api) {
 						'.json',
 						'.ts',
 						'.tsx',
+						'.cjs',
 					],
 					root: ['.'],
 					alias: {
---
+++ metro.config.js
@@ -0,0 +1,38 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+config.resolver.assetExts.push(
+	'obj',
+	'dae',
+	'scn',
+	'zip',
+	'png',
+	'svg',
+	'jpg',
+	'glb',
+	'gltf',
+	'fbx',
+	'lib',
+	'mtl',
+	'bin',
+	'tif',
+	'xpng',
+	'xjpg',
+	'xjpeg',
+);
+config.transformer.getTransformOptions = async () => ({
+	transform: {
+		experimentalImportSupport: false,
+		inlineRequires: true,
+	},
+});
+
+module.exports = config;
