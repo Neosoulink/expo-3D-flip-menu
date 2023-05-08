c86003e2200653d80ca6b54507d970bd181a8ab5
Neosoulink <neodarksoulink@gmail.com>
2023-05-08 11:19:17 +0300

    chore(config): `sourceExts` support

--- metro.config.js
+++ metro.config.js
@@ -9,6 +9,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
+config.resolver.sourceExts.push('js', 'jsx', 'json', 'ts', 'tsx', 'cjs');
 config.resolver.assetExts.push(
 	'obj',
 	'dae',
